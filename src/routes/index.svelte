e19f50480ef51a0d6ed7f388d7321d0d77c6ebdb
Tom 1.618 <103390405+Tom1dot618@users.noreply.github.com>
2022-05-07 16:42:51 +0200

    css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../app.css'
+</script>
+
+<div class="container">
+    <h1>1.618</h1>
+</div>
+
+<style>
+    .container {
+        margin: 1rem;
+    }
+</style>
